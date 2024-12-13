6c28c615c3bddd5971a7b66ab1e5a1f37187f85a
Maimuna <maimunaibrahim808@gmail.com>
2024-12-13 16:04:07 +0100

    added a new action

--- index.js
+++ index.js
@@ -1,88 +1,65 @@
-// const express = require('express')
-// const app = express()
-// const morgan = require('morgan')
 
-// let persons = [
-//     { 
-//       "id": "1",
-//       "name": "Arto Hellas", 
-//       "number": "040-123456"
-//     },
-//     { 
-//       "id": "2",
-//       "name": "Ada Lovelace", 
-//       "number": "39-44-5323523"
-//     },
-//     { 
-//       "id": "3",
-//       "name": "Dan Abramov", 
-//       "number": "12-43-234345"
-//     },
-//     { 
-//       "id": "4",
-//       "name": "Mary Poppendieck", 
-//       "number": "39-23-6423122"
-//     }
-// ];
-// const generateId = () => {
-//   const maxId = persons.length > 0
-//     ? Math.max(...persons.map(n => n.id))
-//     : 0
-//   return maxId + 1
-// }
-
-// app.use(morgan('tiny')); // 'tiny' is one of the predefined formats
-
-// app.get('/', (req, res) => {
-//     res.send('Hello, world!');
-// });
-
-// app.post('/api/persons', (request, response) => {
-//   const body = request.body
-
-//   if (!body.name) {
-//     return response.status(400).json({
-//       error: 'name missing'
-//     })
-//   }
-//   const persons = {
-//     contenet: body.content,
-//     important: body.important || false,
-//     date: new Date(),
-//     id: generateId(),
-//   }
-//   persons = persons.concat(person)
-//   response.json(person)
-// })
-
-
-// app.get('/api/persons', (request, response) => {
-//   response.json(persons)
-// })
-// app.get('/info', (request, response) => {
-//   response.send(`<p>Phonebook has info for ${persons.length} people</p>
-//   <p>${new Date()}</p>`)
-// })
-
-// // Helper function to generate unique IDs
-
-// const PORT = process.env.PORT || 3001
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`)
-// })
 
 
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/notes'
-const cors = require('cors')
+const baseUrl = '/api/notes'
 
-app.use(cors())
 const getAll = () => {
   const request = axios.get(baseUrl)
   return request.then(response => response.data)
 }
 
-// ...
+let notes = [
+  {
+    id: "1",
+    content: "HTML is easy",
+    important: true
+  },
+  {
+    id: "2",
+    content: "Browser can execute only JavaScript",
+    important: false
+  },
+  {
+    id: "3",
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
 
-export default { getAll, create, update }
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => Number(n.id)))
+    : 0
+  return String(maxId + 1)
+}
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
+  response.json(note)
+})
+
+
+
+
+
+const PORT = process.env.PORT || 3001
+ app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+ })
