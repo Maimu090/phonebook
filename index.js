bf16810411f3adc5688f93f9d7aaef3860a366d8
Maimuna <maimunaibrahim808@gmail.com>
2024-12-14 15:35:37 +0100

    added a new action

--- index.js
+++ index.js
@@ -1,69 +1,35 @@
-const express = require('express')
-const app = express()
-const morgan = require('morgan')
+const mongoose = require('mongoose')
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-];
-const generateId = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
-    : 0
-  return maxId + 1
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
 }
 
-app.use(morgan('tiny')); // 'tiny' is one of the predefined formats
+const password = process.argv[2]
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!');
-});
+const url =
+  
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
+mongoose.set('strictQuery',false)
 
-  if (!body.name) {
-    return response.status(400).json({
-      error: 'name missing'
-    })
-  }
-  const persons = {
-    contenet: body.content,
-    important: body.important || false,
-    date: new Date(),
-    id: generateId(),
-  }
-  persons = persons.concat(person)
-  response.json(person)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
 })
 
+const Note = mongoose.model('Note', noteSchema)
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-app.get('/info', (request, response) => {
-  response.send(`<p>Phonebook has info for ${persons.length} people</p>
-  <p>${new Date()}</p>`)
+const note = new Note({
+  content: 'HTML is easy',
+  important: true,
 })
 
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
 // Helper function to generate unique IDs
 
 const PORT = process.env.PORT || 3001
